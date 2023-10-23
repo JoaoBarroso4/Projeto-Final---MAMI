3ba308898c655174dc5025ade1f31284078015f5
Joao <joao.barroso5@aluno.ce.gov.br>
2023-10-23 09:41:02 -0300

    Integração com o JavaScript

--- src/graph.js
+++ src/graph.js
@@ -1,36 +1,40 @@
-function plotGraph() {
-    if (!functionInput) {
-      console.log("Por favor, informe uma função válida.");
-      return;
-    }
-  
-    const data = generateChartData(functionInput);
-    if (chart) {
-      chart.destroy();
-    }
-  
-    const ctx = document.getElementById("chartCanvas").getContext("2d");
-    chart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: data.labels,
-        datasets: [
-          {
-            data: data.values,
-            borderColor: "blue",
-            borderWidth: 2,
-          },
-        ],
-      },
-      options: {
-        animation: false,
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            display: false,
-          },
+function plotGraph(event) {
+  event.preventDefault();
+
+  functionInput = document.getElementById("function").value;
+
+  if (!functionInput) {
+    console.log("Por favor, informe uma função válida.");
+    return;
+  }
+
+  const data = generateChartData(functionInput);
+  if (chart) {
+    chart.destroy();
+  }
+
+  const ctx = document.getElementById("chartCanvas").getContext("2d");
+  chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: data.labels,
+      datasets: [
+        {
+          data: data.values,
+          borderColor: "blue",
+          borderWidth: 2,
+        },
+      ],
+    },
+    options: {
+      animation: false,
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: false,
         },
       },
-    });
-  }
+    },
+  });
+}
--- src/sketch.js
+++ src/sketch.js
@@ -5,8 +5,4 @@ let functionInput;
 function setup() {
   chartCanvas = createCanvas(400, 400);
   chartCanvas.parent("chartCanvas");
-
-  functionInput = "y = x ^ 4 / x ^ 3";
-
-  plotGraph();
 }
